a8e2f83172b81541d7607337dc2d3a5ce4b1e2e5
Michal Rackiewicz <michael.rackiewicz@gmail.com>
2024-05-06 23:45:36 +0200

    Plotting works fine but Date is no diplayed correctly

--- R/eq_map.R
+++ R/eq_map.R
@@ -1,46 +1,21 @@
-library(ggmap)
 library(leaflet)
-
-#
-# API key setup video: https://youtu.be/6jUSyI6x3xg
 
 eq_map <- function(data, annot_col) {
 
-  label_col <- as.character(annot_col)
-  print(label_col)
-
-  p <- leaflet() %>%
-    addTiles() %>%
-    addCircles(data = data,
-               popup = ~ Date,
-               lng =  ~ Longitude,
-               lat = ~ Latitude,
-               radius = ~ Mag * 10000,
-               color = ~ Deaths,
-               fillOpacity = 0.2
+  p <- leaflet::leaflet() %>%
+    leaflet::addTiles() %>%
+    leaflet::addCircles(data = data,
+               popup = ~ str2lang(annot_col),
+               lng =  ~ LONGITUDE,
+               lat = ~ LATITUDE,
+               radius = ~ MAG * 5000,
+               color = ~ "darkred",
+               fillOpacity = 0.4,
+               weight = 1,
                )
   p
 }
 
-mexico %>% eq_map_leaf(annot_col = "Date")
-
-mexico <- eq_clean %>%
-  filter(Country == "MEXICO" & !is.na(Date) & !is.na(Latitude) & !is.na(Longitude))
-
-m <- get_map(c(left = min(mexico$Longitude) - 10, bottom = min(mexico$Latitude) - 3,
-          right = max(mexico$Longitude) + 10, top = max(mexico$Latitude) + 3),
-        source = "stadia", maptype = "stamen_toner_lite", zoom = 5) %>%
-  ggmap(extent = "device") +
-  geom_point(data = mexico, aes(x = Longitude, y = Latitude, color = Deaths, size = Mag, alpha = 0.3))
-m
-
-leaflet() %>%
-  addTiles() %>%
-  addCircles(data = mexico,
-             label = ~ Date,
-             lng =  ~ Longitude,
-             lat = ~ Latitude,
-             radius = ~ 50000,
-             color = ~ Deaths,
-             fillOpacity = 0.4
-  )
+eq_clean %>%
+  filter(COUNTRY == "MEXICO" & lubridate::year(DATE) > 2000) %>%
+  eq_map(annot_col = "DATE")

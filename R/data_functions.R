fba17f579005018923a377b00a305e79a7271505
Michal Rackiewicz <michael.rackiewicz@gmail.com>
2024-05-06 23:28:38 +0200

    changed colnames capitalization

--- R/data_functions.R
+++ R/data_functions.R
@@ -1,20 +1,20 @@
-eq_df <- readr::read_tsv("../data/earthquakes-2024-04-29_16-19-32_+0200.tsv",
+eq_df <- readr::read_tsv("data/earthquakes-2024-04-29_16-19-32_+0200.tsv",
                  col_select = !1)
 
 eq_clean_data <- function(df) {
   df <- df %>%
     janitor::clean_names(.) %>%
-    dplyr::rename_with(stringr::str_to_title) %>%
-    dplyr::filter(!is.na(Location_name)) %>%
+    dplyr::rename_with(stringr::str_to_upper()) %>%
+    dplyr::filter(!is.na(LOCATION_NAME)) %>%
     dplyr::mutate(
-      Date = lubridate::make_date(Year, Mo, Dy),
-      Country = as.factor(stringr::str_extract(Location_name, "(^[a-zA-Z]+)")),
-      Location = stringr::str_remove(Location_name, "(^[a-zA-Z]+)\\W+"),
-      Location = stringr::str_to_title(Location)
+      Date = lubridate::make_date(YEAR, MO, DY),
+      COUNTRY = as.factor(stringr::str_extract(LOCATION_NAME, "(^[a-zA-Z]+)")),
+      LOCATION = stringr::str_remove(LOCATION_NAME, "(^[a-zA-Z]+)\\W+"),
+      LOCATION = stringr::str_to_title(LOCATION)
       ) %>%
-    dplyr::relocate(Date, .before = Hr) %>%
-    dplyr::relocate(c(Country, Location), .before = Location_name) %>%
-    dplyr::select(!c(Location_name))
+    dplyr::relocate(DATE, .before = HR) %>%
+    dplyr::relocate(c(COUNTRY, LOCATION), .before = LOCATION_NAME) %>%
+    dplyr::select(!c(LOCATION_NAME))
 
   df
 }

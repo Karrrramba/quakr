a94e6f7d0ea28be9d1176a140295f75cb13610ae
Michal Rackiewicz <michael.rackiewicz@gmail.com>
2024-05-12 19:05:58 +0200

    Point color scaling correctly

--- R/geom_timeline.R
+++ R/geom_timeline.R
@@ -24,12 +24,8 @@ geom_timeline <- function(mapping = NULL,
                           na.rm = FALSE,
                           show.legend = NA,
                           inherit.aes = TRUE,
-                          ...,
-                          line.params = list(),
-                          point.params = list()) {
-
-  line_params <- modifyList(list(lwd = 0.5, color = "grey", lty = 1), line.params)
-  point_params <- modifyList(list(size = 1.5, color = "lightblue", alpha = 0.3), point.params)
+                          ...
+                          ) {
 
   ggplot2::layer(
     geom = GeomTimeline,
@@ -40,11 +36,12 @@ geom_timeline <- function(mapping = NULL,
     show.legend = show.legend,
     inherit.aes = inherit.aes,
     params = list(na.rm = na.rm,
-                  ...,
-                  line.params = line_params,
-                  point.params = point_params)
+                  ...
+    )
   )
 }
+
+# ggproto ------------------------------------------------------
 
 #' @rdname geom_timeline
 #' @format NULL
@@ -53,23 +50,74 @@ geom_timeline <- function(mapping = NULL,
 GeomTimeline <- ggplot2::ggproto("GeomTimeline", Geom,
                                  required_aes = c("x", "xmin", "xmax"),
                                  default_aes = aes(
-                                   line.params = list(color = "grey", lwd = 0.5, lty = 1),
-                                   point.params = list(color = "lightblue", alpha = 0.3)
+                                   lty = 1,
+                                   lwd = 0.5,
+                                   colour = "grey30",
+                                   size = 1,
+                                   alpha = 0.25
                                  ),
 
                                  draw_key = draw_key_point,
 
-                                 draw_group = function(data, panel_params, coord, line.params, point.params) {
+                                 draw_group = function(data, panel_params, coord, ...) {
 
-                                   print(line.params)
+                                   first_row <- data[1, ]
+                                   print(first_row)
 
-                                   if (!is.null(line.params)) {
-                                     panel_params <- modifyList(panel_params, line.params)
+                                   if (first_row$group >= 1) {
+                                     data$y <- data$group
+                                   } else {
+                                     data$y <- 0.5
                                    }
 
-                                   if (!is.null(point.params)) {
-                                     panel_params <- modifyList(panel_params, point.params)
-                                   }
+                                   coords <- coord$transform(data, panel_params)
+                                   print(coords)
+
+                                   hline_grob <- grid::segmentsGrob(
+                                     x0 = grid::unit(coords$xmin, "npc"),
+                                     y0 = grid::unit(coords$y, "npc"),
+                                     x1 = grid::unit(coords$xmax, "npc"),
+                                     y1 = grid::unit(coords$y, "npc"),
+                                     gp = grid::gpar(
+                                       lwd = first_row$linewidth,
+                                       lty = first_row$linetype
+                                     )
+                                   )
+
+                                   points_grob <- grid::pointsGrob(
+                                     coords$x,
+                                     coords$y,
+                                     pch = 19,
+                                     gp = grid::gpar(
+                                       col = coords$colour,
+                                       alpha = coords$alpha,
+                                       pointsize = coords$size
+                                     )
+                                   )
+
+                                   grid::gList(hline_grob, points_grob)
+                                 }
+)
+
+str(eq_clean)
+eq_clean %>%
+  filter(COUNTRY == "CHINA" & lubridate::year(DATE) >= 1990) %>%
+  ggplot(aes(x = lubridate::year(DATE))) +
+  geom_timeline(aes(xmin = min(lubridate::year(DATE)), xmax = max(lubridate::year(DATE)), size = MAG, colour = TOTAL_DEATHS), lty = "dashed", lwd = 2)
+
+GeomTimeline <- ggplot2::ggproto("GeomTimeline", ggplot2::GeomPoint,
+                                 required_aes = c("x", "xmin", "xmax"),
+                                 default_aes = ggplot2::aes(
+                                   lty = 1,
+                                   lwd = 0.5,
+                                   colour = "grey30",
+                                   size = 1,
+                                   alpha = 0.25
+                                 ),
+
+                                 draw_key = ggplot2::draw_key_point,
+
+                                 draw_group = function(data, panel_params, coord) {
 
                                    first_row <- data[1, ]
 
@@ -87,32 +135,17 @@ GeomTimeline <- ggplot2::ggproto("GeomTimeline", Geom,
                                      x1 = grid::unit(coords$xmax, "npc"),
                                      y1 = grid::unit(coords$y, "npc"),
                                      gp = grid::gpar(
-                                       col = panel_params$line.params$color,
-                                       lwd = panel_params$line.params$lwd * .pt,
-                                       lty = panel_params$line.params$lty
+                                       col = first_row$colour,
+                                       lwd = first_row$lwd,
+                                       lty = first_row$lty
                                      )
                                    )
 
-                                   points_grob <- grid::pointsGrob(
-                                     coords$x,
-                                     coords$y,
-                                     pch = 19,
-                                     gp = grid::gpar(
-                                       col = panel_params$point.params$color,
-                                       alpha = panel_params$point.params$alpha,
-                                       size = panel_params$point.params$size
-                                     )
+                                   points_grob <- ggplot2:::ggproto_parent(ggplot2::geom_point)$draw_group(
+                                     data, panel_params, coord
                                    )
 
                                    grid::gList(hline_grob, points_grob)
                                  }
 )
 
-
-
-
-
-eq_clean %>%
-  filter(COUNTRY == "CHINA" & lubridate::year(DATE) >= 1990) %>%
-  ggplot(aes(x = lubridate::year(DATE))) +
-  geom_timeline(aes(xmin = min(lubridate::year(DATE)), xmax = max(lubridate::year(DATE))))

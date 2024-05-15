16e0a86045562e601d7946b25be16e61fde1159d
Michal Rackiewicz <michael.rackiewicz@gmail.com>
2024-05-15 20:35:21 +0200

    Improved roxygen comments & added package indicator

--- R/eq_location_clean.R
+++ R/eq_location_clean.R
@@ -4,9 +4,10 @@
 #' International earthquake events are assigned to each country individually.
 #' Transforms the location names to title case.
 #'
-#' @param data A data frame.
+#' @param data A data frame with column names with underscores (`_`) instead of
+#' whitespaces and lower letter case
 #'
-#' @importFrom dplyr mutate
+#' @importFrom dplyr across mutate
 #' @importFrom stringr str_to_title
 #' @importFrom tidyr separate_longer_delim separate_wider_delim
 #'
@@ -14,6 +15,7 @@
 #'
 #' @examples
 #' data(mexico)
+#' #uses `clean_names()` from the `janitor` package to tidy up column names
 #' mexico %>%
 #'   janitor::clean_names(.) %>%
 #'   eq_location_clean()
@@ -24,12 +26,19 @@ eq_location_clean <- function(data){
 
     data <- data %>%
       tidyr::separate_longer_delim(., location_name, delim = ";") %>%
-      tidyr::separate_wider_delim(., location_name, names = c("country", "location"),
-                                  delim = ": ", cols_remove = TRUE,
-                                  too_few = "align_start", too_many = "merge") %>%
-      mutate(location = stringr::str_to_title(location),
-             location = gsub('"', '', trimws(location))
-             )
+      tidyr::separate_wider_delim(., location_name,
+                                  names = c("country", "location"),
+                                  delim = ": ",
+                                  cols_remove = TRUE,
+                                  too_few = "align_start",
+                                  too_many = "merge"
+      ) %>%
+      mutate(
+        location = stringr::str_to_title(.),
+        across(c(location, country), ~gsub('"', '', trimws(location))),
+      )
 
-  data
+    data
 }
+
+

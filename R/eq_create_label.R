98826de3562309d2c06d5c60dadb725ff0610476
Michal Rackiewicz <michael.rackiewicz@gmail.com>
2024-05-16 00:41:44 +0200

    Removed redundancy

--- R/eq_create_label.R
+++ R/eq_create_label.R
@@ -4,17 +4,21 @@
 #' Helper function that creates a new information column composed of:
 #' date (in %Y-%m-%d format), magnitude and total number of casualties.
 #' The resulting information are newline separated to keep the labels
-#' adequately small. The labels do not show missing values. Instead, the respecitve
-#' information is ommitted.
+#' adequately small. The labels do not show missing values. Instead, the respective
+#' information is omitted.
 #'
-#' @param data description
+#' @param data A character vector containing the columns "location', 'mag' and
+#' 'total_deaths'.
+#'
+#' @returns A data frame with HTML-formatted label information.
 #'
 #' @examples
 #' data(mexico)
 #'
+#'
+#' #create and use labels in a plot
 #' mexico %>%
 #'   eq_clean_data(.) %>%
-#'   filter(year(date) > 2000) %>%
 #'   mutate(popup_text = eq_create_label(.))
 #'   eq_map(annot_col = "popup_text")
 #'
@@ -22,11 +26,12 @@
 
 eq_create_label <- function(data) {
 
-  df <- paste(
+  l <- paste(
       ifelse(!is.na(data$location), paste("<b>Location:</b>", data$location, "<br/>"), ""),
       ifelse(!is.na(data$mag), paste("<b>Magnitude:</b>", data$mag, "<br />"), ""),
       ifelse(!is.na(data$total_deaths), paste("<b>Total deaths</b>:", data$total_deaths), "")
       )
 
-  df
+  l
+
 }

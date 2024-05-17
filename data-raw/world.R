0759eae4825347ad23d97eb0b2d5758d839e7ba2
Michal Rackiewicz <michael.rackiewicz@gmail.com>
2024-05-17 18:25:33 +0200

    Removed descriptions and total info

--- data-raw/world.R
+++ data-raw/world.R
@@ -3,10 +3,11 @@ library(readr)
 library(tidyr)
 
 raw <- readr::read_tsv("data-raw/earthquakes-2024-04-29_16-19-32_+0200.tsv",
-                         col_select = !1)
+                       col_select = !c(1, contains("Description"), contains("Total")))
+
 world <- raw %>%
   filter(if_all(c(Year, Mag, Latitude, Longitude, 'Location Name'), ~!is.na(.))) %>%
-  separate_longer_delim(., 'Location Name', delim = ";") %>%
+  separate_longer_delim(., 'Location Name', delim = ";")
 
 write_csv(world, "data-raw/world.csv")
 save(world, file = "data-raw/world.rda")

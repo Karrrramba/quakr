9d4f043caed606de8dae3dd7ec2dadc2cf261651
Michal Rackiewicz <michael.rackiewicz@gmail.com>
2024-05-26 16:32:05 +0200

    Color and provider customization

--- R/eq_map.R
+++ R/eq_map.R
@@ -2,15 +2,18 @@
 #'
 #' @description
 #' \code{eq_map} plots the locations of earthquake events on a geographical map
-#'   of one or multiple countries. Earthquake locations are plotted as
-#'   red circles with labels and size scaled based on magnitude.
+#'     of one or multiple countries. Earthquake locations are plotted as
+#'     red circles with labels and size scaled based on magnitude.
 #'
 #' @param data A data frame containing columns for "country",
-#'   "magnitude", "longitude" and "latitude". The latter two must be values in [deg].
-#'
+#'     "magnitude", "longitude" and "latitude". The latter two must be values in [deg].
 #' @param annot_col A vector of values which will be used for labels.
 #' @param scaling_factor A numeric value which scales the point size.
-#'   Defaults to 1.
+#'     Defaults to 1.
+#' @param color Character value specifying the color of the Earthquake locations.
+#'     Defaults to "darkred".
+#' @param provider Name of the map provider. See: https://leaflet-extras.github.io/leaflet-providers/preview/) for available options.
+#'     Defaults to "Esri.WorldTopoMap".
 #'
 #' @returns An interactive leaflet map.
 #'
@@ -30,16 +33,16 @@
 #'
 #' @export
 
-eq_map <- function(data, annot_col, scaling_factor = 1) {
+eq_map <- function(data, annot_col, scaling_factor = 1, color = "darkred", provider = "Esri.WorldTopoMap") {
 
   leaflet::leaflet() %>%
-    leaflet::addProviderTiles("Esri.WorldTopoMap") %>%
+    leaflet::addProviderTiles(provider) %>%
     leaflet::addCircles(data = data,
                popup = ~ data[[annot_col]],
                lng =  ~ longitude,
                lat = ~ latitude,
                radius = ~ mag * (scaling_factor * 1000),
-               color = ~ "darkred",
+               color = ~ color,
                fillOpacity = 0.4,
                weight = 1,
                )
